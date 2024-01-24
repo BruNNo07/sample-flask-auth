522a7014c735c723235ff8624efce07337bd9ee1
BruNNo07 <brunno.dallafg@gmail.com>
2024-01-24 14:50:45 -0300

    rota de logout

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from loginManager import login_manager
+from flask_login import login_user, current_user, logout_user,login_required
 
 from models.user import User
 
@@ -12,6 +13,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db.init_app(app)
 login_manager.init_app(app)
 
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 @app.route("/login", methods=["POST"])
 def login():
     data = request.json
@@ -22,14 +29,18 @@ def login():
         user = User.query.filter_by(username=username).first()
 
         if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)
             return jsonify({'message': 'Usuario autenticado'})
         
 
     return jsonify({'message': 'Credenciais inválidas'}), 400
 
-@app.route("/hello-world", methods=['GET'])
-def hello_world():
-    return "hello world"
+@app.route("/logout", methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({'message': 'Logout realizado'})
 
 if __name__ == '__main__':
     app.run(debug=True)
