1c3a60f8d8e8316107e666a4b4b393cc4683ef3e
BruNNo07 <brunno.dallafg@gmail.com>
2024-01-26 11:32:58 -0300

    trocando banco de dados para mysql

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from database import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@127.0.0.1:3306/flask-crud'
 
 db.init_app(app)
 login_manager.init_app(app)
@@ -78,19 +78,20 @@ def update_user(id):
     
     return jsonify({'message': 'Usuário não encontrado.'}), 404
 
-@app.route("/user/<int:id>",methods={"DELETE"})
-def update_user(id):
-    user = User.query.get(id)
+@app.route('/user/<int:id>', methods=["DELETE"])
+@login_required
+def delete_user(id):
+  user = User.query.get(id)
 
-    if id != current_user.id:
-        return jsonify({'message': 'Deleção não permitida'}), 403
+  if id == current_user.id:
+    return jsonify({"message": "Deleção não permitida"}), 403
 
-    if user:
-        db.session.delete(user)
-        db.session.commit()
-        return jsonify({'message': 'Usuário deletado.'})
-    
-    return jsonify({'message': 'Usuário não encontrado.'}), 404
+  if user:
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({"message": f"Usuário {id} deletado com sucesso"})
+  
+  return jsonify({"message": "Usuario não encontrado"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
